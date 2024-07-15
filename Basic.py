5bcdbeb05ddd13531b6e73c2c486ef78217ed546
Woost-Phi <jhon.felix098@gmail.com>
2024-07-15 13:54:19 -0300

    Dia 2

---
+++ Basic.py
@@ -0,0 +1,4 @@
+n1 = input('Qual ano você nasceu?')
+n2 = input('Qual ano nasceu a sua Mãe?')
+soma = n2 - n1
+print('A diferença de idade entre {} e {} é de {}!' .format(n2,n1, soma))
--- Caculadora.py
+++ Caculadora.py
@@ -1,6 +1,18 @@
 import Caculadora
 import math
+import sys
 
+import os
+import platform
+import tkinter
+
+import tkinter as tk
+from tkinter import Menu. False
+
+
+from functools import partial
+from json import load as json_load
+from json import dump as json_dump
 
 class Calculador (object):
         def calculation(self, calc):
@@ -8,7 +20,7 @@ class Calculador (object):
         def calculation_validation (self, calc):
             try: 
                result = eval(calc)
-               return self. __format_result (result=result)
+               return self. __format_result (result= result)
             except (NameError, ZeroDivisionError, SystemError, ValueError):
                 return 'Erro'         
 def __format_result(self, result):
