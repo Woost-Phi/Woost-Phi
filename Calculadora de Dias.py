78876ad24b17f83b0105a4c409e0a0d6d1219e04
Woost-Phi <jhon.felix098@gmail.com>
2024-07-15 17:19:14 -0300

    Update Calculadora de Dias.py

--- Calculadora de Dias.py
+++ Calculadora de Dias.py
@@ -1,4 +1,45 @@
-n1 = int(input('Qual ano você nasceu?'))
-n2 = int(input('Qual ano nasceu a sua Mãe?'))
-soma = n1 - n2
-print('A diferença de idade entre {} e {} é de {}!' .format(n2,n1, soma))
+D1= int(input('Digite o dia do MÊS que iniciará a contagem: '))
+if D1>31:
+        print('Data incorreta)')
+        
+else:
+            D1<=31
+    
+D2= int(input('Digite o Mês: '))
+
+if D2>12:
+        print('Mês invalido)')
+        
+else:
+            D1<=12
+D3= int(input('Digite o ano de inicio: '))
+D4= int(input('Digite o dia que finaliza a contagem: '))
+if D4>31:
+        print('Data incorreta)')
+        
+else:
+            D4<=31
+    
+D5= int(input('Digite o mês que finaliza a contagem: '))
+if D5>12:
+        print('Mês invalido)')
+        
+else:
+            D5<=12
+D6= int(input('Digite o ano que finaliza a contagem: '))
+
+if D1 == 1 or 3 and 5 or 7 and 8 or 9:   
+        DF1 = 31 - D1 
+else: DF1 = 2 or 4 and 6 or 10 and 11
+
+if D1== 2:
+        DF1= 28 - D1
+else: D1 != 2
+if D1 == 4 or 6 and 10 or 11:   
+        DF1 = 31 - D1 
+else: DF1 != 4 or 6 and 10 or 11
+      
+
+DF2= DF1 +  
+
+
